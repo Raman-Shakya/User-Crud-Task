65f408736561ef19befe7f209227bd03af3f1954
Raman-Shakya <raman.gwarko15@gmail.com>
2024-12-18 21:13:14 +0545

    CRUD business logic implementation

--- backend/user/controller/controller.js
+++ backend/user/controller/controller.js
@@ -1,26 +1,44 @@
-const userCollection = require('../model/model');
+const userModel = require('../model/model');
 
+// get all user records
 const getRequest = (req, res) => {
-    const { userName, password } = req.query;
-    res.send('hello world');
+    userModel.find()
+        .then((r) => res.send(r))
+        .catch((e) => res.status(404).send("Data not found" + e));
 }
 
+// create new user
 const postRequest = (req, res) => {
     const { userName, password } = req.body;
-    res.json({
+    userModel.create({
         userName,
         password
-    });
+    })
+        .then((r) => res.json(r))
+        .catch((e) => res.status(403).send("Error" + e));
 }
 
+// update password for given user
 const putRequest = (req, res) => {
     const { userName, password } = req.body;
-    res.send("hello world");
+    userModel.updateOne({
+        userName: userName
+    }, {
+        password: password
+    })
+        .then((r) => res.json(r))
+        .catch((e) => res.status(403).send("Forbidden action"));
 }
 
+// delete user
 const deleteRequest = (req, res) => {
     const { userName, password } = req.body;
-    res.send("hello world");
+    userModel.deleteOne({
+        userName: userName,
+        password: password
+    })
+        .then((r) => res.json(r))
+        .catch((e) => res.status(403).send("Forbidden action"));
 }
 
 module.exports = [getRequest, postRequest, putRequest, deleteRequest]
--- backend/user/model/model.js
+++ backend/user/model/model.js
@@ -1,9 +1,22 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 
-const uri = "mongodb://localhost:27017/";
-const client = new MongoClient(uri);
+const uri = "mongodb://localhost:27017/userdb";
+mongoose.connect(uri);
+// const client = new MongoClient(uri);
 
-const db = client.db('userdb');
-const user = db.collection('user');
+// const db = client.db('userdb');
+// const user = db.collection('user');
 
-module.exports = user;
+const userSchema = new mongoose.Schema({
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+})
+
+module.exports = mongoose.model('User', userSchema);
